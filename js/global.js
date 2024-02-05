7904d3f5b010af28de034d7cef7523b2910a2e37
Bradley Waye <138162220+bradleywaye@users.noreply.github.com>
2024-02-05 16:03:45 -0500

    add js logic to focus the search box

--- js/global.js
+++ js/global.js
@@ -9,7 +9,15 @@
 
   Drupal.behaviors.cu_theme = {
     attach: function (context, settings) {
-
+      var collapseSearch = document.getElementById('CollapseSearch');
+      if (collapseSearch) {
+        collapseSearch.addEventListener('shown.bs.collapse', function () {
+          var inputField = document.getElementById('edit-keys');
+          if (inputField) {
+            inputField.focus();
+          }
+        });
+      }
     }
   };
 
