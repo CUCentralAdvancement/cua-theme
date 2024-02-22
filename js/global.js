5fe148ea8faed324a4a598b0b8447637c67b1a86
Bradley Waye <138162220+bradleywaye@users.noreply.github.com>
2024-02-22 13:50:54 -0500

    update how the js fires and where

--- js/global.js
+++ js/global.js
@@ -7,6 +7,9 @@
 
   'use strict';
 
+  /**
+   * show search field
+   */
   Drupal.behaviors.cu_theme = {
     attach: function (context, settings) {
       var collapseSearch = document.getElementById('CollapseSearch');
@@ -20,5 +23,32 @@
       }
     }
   };
+
+  /**
+   * animations viewpoint
+   */
+  document.addEventListener("DOMContentLoaded", function() {
+    const elements = document.querySelectorAll(".animate-trigger");
+
+    function handleAnimations(entries, observer) {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          const animationClasses = entry.target.dataset.animationClasses;
+          if (animationClasses) {
+            animationClasses.split(" ").forEach(className => {
+              entry.target.classList.add(className);
+            });
+          }
+          observer.unobserve(entry.target);
+        }
+      });
+    }
+
+    const observer = new IntersectionObserver(handleAnimations, { threshold: 0.1 });
+
+    elements.forEach(element => {
+      observer.observe(element);
+    });
+  });
 
 })(Drupal);
