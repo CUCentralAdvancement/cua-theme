a5c4be278f65f09f0830b243feaf8d2cf0afea8a
Bradley Waye <138162220+bradleywaye@users.noreply.github.com>
2024-03-05 15:04:35 -0500

    update js for effeciency

--- js/global.js
+++ js/global.js
@@ -7,66 +7,47 @@
 
   'use strict';
 
-  /**
-   * show search field
-   */
-  Drupal.behaviors.cu_theme = {
-    attach: function (context, settings) {
-      var collapseSearch = document.getElementById('CollapseSearch');
-      if (collapseSearch) {
-        collapseSearch.addEventListener('shown.bs.collapse', function () {
-          var inputField = document.getElementById('edit-keys');
-          if (inputField) {
-            inputField.focus();
-          }
-        });
-      }
-    }
-  };
-
-  /**
-   * Sticky Menu
-   */
+  // Cache frequently accessed elements
   var navbar = document.getElementById('navbar-main');
+  var collapseSearch = document.getElementById('CollapseSearch');
+
+  // Sticky navigation
   var sticky = navbar.offsetTop;
 
   function stickyNav() {
-    if (window.pageYOffset >= sticky) {
-      navbar.classList.add("sticky");
-    } else {
-      navbar.classList.remove("sticky");
-    }
+    navbar.classList.toggle('sticky', window.pageYOffset >= sticky);
   }
 
-  window.onscroll = function() {
-    stickyNav();
-  };
+  window.addEventListener('scroll', stickyNav);
 
-  /**
-   * animations viewpoint
-   */
-  document.addEventListener("DOMContentLoaded", function() {
-    const elements = document.querySelectorAll(".animate-trigger");
+  // Show search field
+  if (collapseSearch) {
+    collapseSearch.addEventListener('shown.bs.collapse', function () {
+      var inputField = document.getElementById('edit-keys');
+      if (inputField) {
+        inputField.focus();
+      }
+    });
+  }
 
-    function handleAnimations(entries, observer) {
-      entries.forEach(entry => {
+  // Animations on viewport
+  document.addEventListener('DOMContentLoaded', function () {
+    var observer = new IntersectionObserver(handleAnimations, { threshold: 0.1 });
+
+    document.querySelectorAll('.animate-trigger').forEach(function (element) {
+      observer.observe(element);
+    });
+
+    function handleAnimations(entries) {
+      entries.forEach(function (entry) {
         if (entry.isIntersecting) {
-          const animationClasses = entry.target.dataset.animationClasses;
+          var animationClasses = entry.target.dataset.animationClasses;
           if (animationClasses) {
-            animationClasses.split(" ").forEach(className => {
-              entry.target.classList.add(className);
-            });
+            entry.target.classList.add(...animationClasses.split(' '));
           }
           observer.unobserve(entry.target);
         }
       });
     }
-
-    const observer = new IntersectionObserver(handleAnimations, { threshold: 0.1 });
-
-    elements.forEach(element => {
-      observer.observe(element);
-    });
   });
-
 })(Drupal);
