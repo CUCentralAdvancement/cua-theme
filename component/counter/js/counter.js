519a2c633517904261e02012bc31b2e2277bf6d0
Bradley Waye <138162220+bradleywaye@users.noreply.github.com>
2024-04-16 16:39:12 -0400

    update title as it was killing the contex edit

--- component/counter/js/counter.js
+++ component/counter/js/counter.js
@@ -4,15 +4,15 @@
   Drupal.behaviors.cuaCounterUpWaypoints = {
     attach: function (context, settings) {
       // Select all elements with the counter-up class
-      var $counterUpElements = $('.counter-up', context);
+      const $counterUpElements = $('.counter-up', context);
 
       // Iterate through each counter-up element
       $counterUpElements.each(function(index, element) {
-        var $this = $(element);
+        const $this = $(element);
         // Create a waypoint for the current element
-        var waypoint = new Waypoint({
+        const waypoint = new Waypoint({
           element: $this[0],
-          handler: function(direction) {
+          handler: function (direction) {
             if (direction === 'down') {
               console.log('Waypoint triggered for element:', $this);
               // Animate the current element
@@ -21,13 +21,13 @@
               this.destroy();
             }
           },
-          offset: 'bottom-in-view'
+          offset: 'bottom-in-view',
         });
       });
 
       // Function to animate counter
       function animateCounter($element) {
-        var countTo = parseFloat($element.data('count'));
+        const countTo = parseFloat($element.data('count'));
         if (!isNaN(countTo)) {
           $({ countNum: 0 }).animate({
                 countNum: countTo
