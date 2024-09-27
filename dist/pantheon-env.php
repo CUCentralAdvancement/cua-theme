50cdf0cc0ca00119e45fe1bd47abb6531a62371b
Bradley Waye <bradleywaye@users.noreply.github.com>
2024-09-27 11:09:53 -0400

    self host fa

---
+++ dist/pantheon-env.php
@@ -0,0 +1,8 @@
+<?php
+/*echo '<h2>_SERVER</h2>';
+ksort($_SERVER, SORT_STRING);
+echo '<pre>'; var_dump($_SERVER); echo '</pre>';
+echo '<h2>_ENV</h2>';
+ksort($_ENV, SORT_STRING);
+echo '<pre>'; var_dump($_ENV); echo '</pre>';*/
+?>
